4060d76718b30c69c83f2ee2f47e5600f4050640
Joe Gomez <jggomez88@gmail.com>
2020-04-30 19:44:10 -0700

    adds click events, data filter, and search function

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
   Please add all Javascript code to this file.
 */
 
-var url = 'http://newsapi.org/v2/top-headlines?' +
+const url = 'http://newsapi.org/v2/top-headlines?' +
   'country=us&' +
   'apiKey=' + newsAPIKey;
+
+const sources = [];
 
 var req = new Request(url);
 fetch(req)
@@ -16,55 +18,45 @@ fetch(req)
   updateSourcesList(data);
   updateArticleMenu(data);
   addClickEvents(data);
+  filterDataSource(data);
 
 });
 
 // Updates sources list in drop down menu by creating new li w/ a
 const updateSourcesList = (data) => {
 
-  let sources = [];
-
   for (let i=0;i<data.articles.length;i++) {
     let source = data.articles[i].source.name
-    sources.push(source);
+    sources.unshift(source);
   }
 
   let uniqueSources = Array.from(new Set(sources));
-  uniqueSources = uniqueSources.slice(0, 10)
 
-  for (let i=0;i<uniqueSources.length;i++) {
+  for (let i=0;i<12;i++) {
     $('.source-list').append(`<li><a href="#">${uniqueSources[i]}</a></li>`);
   }
-
-  $('.source-list li a').click((e) => {
-    let clickedSource = e.currentTarget.innerHTML
-    if (sources.includes(clickedSource)) {
-      console.log('true');
-    }
-
-  });
-
-
-  console.log(sources);
-  console.log(uniqueSources);
+  // Adds view all to end of source menu
+  $('.source-list').append(`<li><a href="#">VIEW ALL</a></li>`);
 
 }
 
 // Updates article menu with titles, url, etc
 const updateArticleMenu = (data) => {
 
-  for (let i=0;i<data.articles.length;i++) {
-    const title = data.articles[i].title;
+  const articleData = data.articles;
+
+  for (let key in articleData) {
+    const title = articleData[key].title;
     const formatTitle = title.split(' - ')[0];
-    const source = data.articles[i].source.name;
-    const image = data.articles[i].urlToImage;
+    const source = articleData[key].source.name;
+    const image = articleData[key].urlToImage;
     const views = (Math.random(100) * 10000).toFixed(0);
 
     const articleHTML = `
     <section class="featuredImage">
       <img src=${image} alt="" />
     </section>
-    <section class="articleContent" id=${i} >
+    <section class="articleContent" id=${key} >
       <a href='#'><h3>${formatTitle}</h3></a>
       <h6>${source}</h6>
     </section>
@@ -74,10 +66,10 @@ const updateArticleMenu = (data) => {
     <div class="clearfix"></div>
     `
 
-    $('#main').append('<article class="article"></article>')
+    $('#main').append(`<article id=${key} class="article"></article>`)
 
-    let $article = $('.article')[i]
-    $article.innerHTML = articleHTML;
+    let $article = $(`#${key}.article`);
+    $article[0].innerHTML = articleHTML;
 
   }
 }
@@ -96,21 +88,97 @@ const addClickEvents = (data) => {
 
     $('div.container h1').text(formatTitle);
     $('.container p').text(description);
-    $('a.popUpAction').attr(`ahref, ${url}`)
+    $('a.popUpAction').attr('href', url);
 
   $('#popUp').removeClass('hidden loader');
-    // .appendTo( $(`#${id}.articleContent`) );
   });
 
   $('.closePopUp').click(() => {
     $('#popUp').addClass('hidden');
   });
+
+}
+
+
+ // Filters data based on selected source via dropdown click and search submit
+const filterDataSource = (data) => {
+
+  const articleData = data.articles;
+  const $search = $('#search');
+
+  // Activates search bar on search image click and filters data on search submit
+
+  $('#search a img').click(() => {
+
+    const filterData = {articles:{}}
+
+    $search.toggleClass('active');
+    const $searchInput = $('#search input');
+
+    $searchInput.on('change', (e) => {
+      let userSearch = e.target.value
+
+      for (let key in articleData) {
+        if(articleData[key].source.name === userSearch) {
+          filterData.articles[key] = articleData[key]
+          $('.article').remove();
+          updateArticleMenu(filterData);
+          addClickEvents(filterData);
+        }
+        if(userSearch === '') {
+          $('.article').remove();
+          updateArticleMenu(data);
+          addClickEvents(data);
+          $search.removeClass('active');
+        }
+    }
+
+    });
+
+  });
+
+// filters data on source dropdown menu click
+   $('.source-list li a').click((e) => {
+
+    $('.article').remove();
+
+    const filterData = {articles:{}}
+
+    let clickedSource = e.currentTarget.innerHTML;
+
+    for (let key in articleData) {
+      if(articleData[key].source.name === clickedSource) {
+        filterData.articles[key] = articleData[key]
+    }
+  }
+
+  if(clickedSource === 'VIEW ALL') {
+    $('.drop-down a span').text('VIEW ALL');
+    updateArticleMenu(data);
+    addClickEvents(data);
+  } else {
+    $('.drop-down a span').text(clickedSource);
+    updateArticleMenu(filterData);
+    addClickEvents(filterData);
+  }
+
+  });
+
+
 }
 
 
 
+// Parking Lot
+
+// let $articleSources = $('.articleContent h6').text();
 
 
+// $articleSources.filter(function() {
+//   if ($(this).text() !== clickedSource) {
+//     $(this).parentsUntil('#main').css("visibility", "hidden");
+//   }
+// });
 
 
 // const sources = [
